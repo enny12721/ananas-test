050768f46530571a2c5675d161ca2a13ce1723c5
Осинцева Ксения (практика) <enny12721@gmail.com>
2022-07-18 13:46:38 +0500

    Исправил форму удаления сообщения

--- resources/views/wall/message.blade.php
+++ resources/views/wall/message.blade.php
@@ -4,8 +4,9 @@
     </p>
     <p class="h6"> {{ $message->text }}</p>
     @if($message->user == auth()->user())
-        <form action="{{ route('message.destroy', $message) }}" method="DELETE">
+        <form action="{{ route('message.destroy', $message) }}" method="POST">
             @csrf
+            @method("DELETE")
             <button type="success" class="btn btn-danger"> Удалить </button>
         </form>
     @endif
